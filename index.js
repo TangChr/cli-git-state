4e8ca4a9e8c712408e7440bc74e33acb49d10b90
Christian Tang <git@tcdev.dk>
2017-07-19 13:37:35 +0200

    standard code style

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
-var gitState = require('git-state');
+var gitState = require('git-state')
 
 var path = '.'
- 
+
 gitState.isGit(path, function (exists) {
   if (!exists) return
- 
   gitState.check(path, function (err, result) {
     if (err) throw err
-    console.log(result) // => { branch: 'master', 
-                        //      ahead: 0, 
-                        //      dirty: 9, 
-                        //      untracked: 1, 
-                        //      stashes: 0 } 
+    console.log(result) // => { branch: 'master',
+                        //      ahead: 0,
+                        //      dirty: 9,
+                        //      untracked: 1,
+                        //      stashes: 0 }
   })
 })
